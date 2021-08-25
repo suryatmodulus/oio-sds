7d9137410ec8707731c9d3cf663ecbd2e769f0d2
Lhoussain Ait Assou <lhoussain.ait-assou@ovhcloud.com>
2021-08-25 19:02:33 +0200

    Account with backend for fdb Rewrite account using foundationdb as backend

--- oio/common/green.py
+++ oio/common/green.py
@@ -33,7 +33,7 @@ from eventlet.green.httplib import (HTTPConnection, HTTPSConnection, # noqa
 from eventlet.queue import Empty, LifoQueue, LightQueue # noqa
 from eventlet.semaphore import Semaphore # noqa
 
-eventlet.monkey_patch(os=False)
+eventlet.monkey_patch(os=False, thread=False)
 
 logging.thread = eventlet.green.thread
 logging.threading = threading
--- tools/oio-bootstrap.py
+++ tools/oio-bootstrap.py
@@ -1331,6 +1331,8 @@ fall: 1
 include_dir: ${CFGDIR}/watch
 """
 
+FDB = str(os.environ['HOME']) + '/.local/etc/foundationdb'
+
 template_account = """
 [account-server]
 bind_addr = ${IP}
@@ -1342,6 +1344,12 @@ log_facility = LOG_LOCAL0
 log_level = INFO
 log_address = /dev/log
 syslog_prefix = OIO,${NS},${SRVTYPE},${SRVNUM}
+
+backend_type = fdb
+# default fdb file /etc/foundationdb/fdb.cluster will be used if fdb_file
+# is not defined
+
+fdb_file = ${FDB}/fdb.cluster
 
 # Let this option empty to connect directly to redis_host
 #sentinel_hosts = 127.0.0.1:26379,127.0.0.1:26380,127.0.0.1:26381
@@ -1656,7 +1664,8 @@ def generate(options):
                WEBHOOK=WEBHOOK,
                WEBHOOK_ENDPOINT=WEBHOOK_ENDPOINT,
                TLS_CERT_FILE=TLS_CERT_FILE,
-               TLS_KEY_FILE=TLS_KEY_FILE)
+               TLS_KEY_FILE=TLS_KEY_FILE,
+               FDB=FDB)
 
     def merge_env(add):
         env = dict(ENV)
